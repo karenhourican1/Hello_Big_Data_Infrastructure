61f8f09a8285b5d6beeeeb8706c14916b0101f09
Karen Hourican <karenhourican1@gmail.com>
2023-12-12 16:30:43 +0100

    Re-running each function and removing unnecessary comments

--- bdi_api/s1/exercise.py
+++ bdi_api/s1/exercise.py
@@ -64,7 +64,7 @@ def download_data() -> str:
             with open(file_path, 'wb') as f:
                 f.write(response.content)
 
-            # Now, you may want to decompress it right away
+            # Decompress the files
             with gzip.open(file_path, 'rb') as f_in:
                 with open(file_path.with_suffix(''), 'wb') as f_out:  # Removes the .gz suffix
                     shutil.copyfileobj(f_in, f_out)
@@ -81,19 +81,6 @@ def download_data() -> str:
             print(f"An error occurred: {err}")
 
     return "OK"
-    # data_dir = Path(settings.raw_dir) / "day=20231101"
-    # if not data_dir.exists():
-    #     return "Data directory does not exist."
-    #
-    # # Initialize an empty DataFrame to hold all the data
-    # all_data = pd.DataFrame()
-    #
-    # # Process each JSON file
-    # for json_file in data_dir.glob('*.json'):
-    #     # Read the file into a DataFrame
-    #     df = pd.read_json(json_file)
-    #     # Append to the all_data DataFrame
-    #     all_data = all_data.append(df, ignore_index=True)
 
 
 @s1.post("/aircraft/prepare")
@@ -118,15 +105,11 @@ def prepare_data() -> str:
     download_dir = os.path.join(settings.raw_dir, "day=20231101")
     prepared_dir = os.path.join(settings.prepared_dir,"day=20231101")
 
-    print(f"Download Directory: {download_dir}")
-    print(f"Output Directory: {prepared_dir}")
-
     # Process each file
     filenames = glob.glob(f'{download_dir}/*.json')
     print(f"Found {len(filenames)} files to process")
 
     for filename in filenames:
-        print(f"Processing file: {filename}")
         with open(filename, 'r') as file:
             data = json.load(file)
         aircraft_data = data.get("aircraft", [])
@@ -153,67 +136,8 @@ def prepare_data() -> str:
         with open(output_filename, 'w') as output_file:
             json.dump(extracted_data, output_file, indent=4)
 
-        print(f"Data written to {output_filename}")
-
     return "OK"
 
-
-
-    # data_dir = Path(settings.raw_dir) / "day=20231101"
-    # prepared_dir = data_dir / "prepared"
-    # local_data = Path(settings.local_dir)
-
-
-    # Clean the prepared directory
-    # if prepared_dir.exists():
-    #     shutil.rmtree(prepared_dir)
-    # prepared_dir.mkdir()
-    #
-    # # Initialize an empty DataFrame to store all data
-    # dataframe_total = pd.DataFrame()
-    #
-    # # Read and process each JSON file
-    # for json_file in local_data.glob('*.json'):
-    #     df = pd.read_json(json_file)
-    #     df = df['aircraft']
-    #     df = pd.DataFrame([[k, *v] for k, v in df.items()],
-    #                       columns=['hex', 'type', 'flight' 'r', 't', 'alt_baro', 'gs', 'lat', 'lon', 'alt_baro', 'gs', 'emergency'])
-    #     #print(df.info())
-    #    # "hex": "a65800", "type": "adsc", "flight": "DL295   ", "r": "N508DN", "t": "A359", "alt_baro": 39996, "gs": 454.0, "track": 244.71, "baro_rate": -16, "lat": 46.577740, "lon": -178.413162, "nic": 0, "rc": 0, "seen_pos": 190.091, "alert": 0, "spi": 0, "mlat": [], "tisb": [], "messages": 31181264, "seen": 190.1, "rssi": -49.5
-    #
-    #     # Select only the required columns
-    #   #  df = df[['hex', 'r', 't', 'lat', 'lon', 'alt_baro', 'gs', 'emergency']]
-    #
-    #     # Rename columns to desired names
-    #     df.rename(columns={
-    #         'hex': 'icao',
-    #         'r': 'registration',
-    #         't': 'type',
-    #         'lat': 'lat',
-    #         'lon': 'lon',
-    #         'alt_baro': 'altitude_baro',
-    #         'gs': 'ground_speed',
-    #     }, inplace=True)
-    #     print(df)
-    #     return "OK"
-    #
-    #     # Create "had_emergency" column
-    #     df["had_emergency"] = df["emergency"].apply(lambda e: e not in ["none", None])
-    #
-    #     # Convert "now" column to datetime and assign it to "timestamp" column
-    #     df["timestamp"] = pd.to_datetime(df["timestamp"], unit='s')
-    #
-    #     # Append the processed DataFrame to the total DataFrame
-    #     dataframe_total = pd.concat([dataframe_total, df], ignore_index=True)
-    #     print(dataframe_total)
-    #     print("hello world")
-    #     # Partition by aircraft type
-    #     for aircraft_type, group in dataframe_total.groupby('type'):
-    #         type_dir = prepared_dir / str(aircraft_type)
-    #         type_dir.mkdir(parents=True, exist_ok=True)
-    #         group.to_csv(type_dir / f"{json_file.stem}_prepared.csv", index=False)
-
-    #return "OK"
 
 @s1.get("/aircraft/")
 def list_aircraft(num_results: int = 100, page: int = 0) -> list[dict]:
@@ -268,19 +192,16 @@ def get_aircraft_position(icao: str, num_results: int = 1000, page: int = 0) -> 
     positions = []
 
     for json_file in prepared_dir.glob("*.processed.json"):
-        print(f"Processing file: {json_file}")
         try:
             with open(json_file, 'r') as file:
                 data = json.load(file)
                 for record in data:
-                    print(f"Current record icao: {record['icao']}, Input icao: {icao}")
                     if record["icao"] == icao:
                         positions.append({
                             "timestamp": record["timestamp"],
                             "lat": record["latitude"],
                             "lon": record["longitude"]
                         })
-                        print(f"Added position: {positions[-1]}")
         except json.decoder.JSONDecodeError as e:
             print(f"Error reading file {json_file}: {e}")
             continue
@@ -343,21 +264,21 @@ def get_aircraft_statistics(icao: str) -> dict:
     }
 
 
-def clean_data_directories():
-    raw_dir = Path(settings.raw_dir) / "day=20231101"
-    prepared_dir = Path(settings.prepared_dir) / "day=20231101"
-
-    # Delete files in the raw data directory
-    if raw_dir.exists() and raw_dir.is_dir():
-        shutil.rmtree(raw_dir)
-        print(f"Deleted all files in {raw_dir}")
-
-    # Delete files in the prepared data directory
-    if prepared_dir.exists() and prepared_dir.is_dir():
-        shutil.rmtree(prepared_dir)
-        print(f"Deleted all files in {prepared_dir}")
-
-    # Recreate the empty directories
-    raw_dir.mkdir(parents=True, exist_ok=True)
-    prepared_dir.mkdir(parents=True, exist_ok=True)
-    print("Recreated empty directories")
+# def clean_data_directories():
+#     raw_dir = Path(settings.raw_dir) / "day=20231101"
+#     prepared_dir = Path(settings.prepared_dir) / "day=20231101"
+#
+#     # Delete files in the raw data directory
+#     if raw_dir.exists() and raw_dir.is_dir():
+#         shutil.rmtree(raw_dir)
+#         print(f"Deleted all files in {raw_dir}")
+#
+#     # Delete files in the prepared data directory
+#     if prepared_dir.exists() and prepared_dir.is_dir():
+#         shutil.rmtree(prepared_dir)
+#         print(f"Deleted all files in {prepared_dir}")
+#
+#     # Recreate the empty directories
+#     raw_dir.mkdir(parents=True, exist_ok=True)
+#     prepared_dir.mkdir(parents=True, exist_ok=True)
+#     print("Recreated empty directories")
