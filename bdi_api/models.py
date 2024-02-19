9b0c700da8a3b6a7e38cd9d2ca33e422df36247f
Karen Hourican <karenhourican1@gmail.com>
2024-02-19 17:31:44 +0100

    Updated model column names to match names in database schema

--- bdi_api/models.py
+++ bdi_api/models.py
@@ -1,5 +1,3 @@
-# bdi_api/models.py
-
 from sqlalchemy import Column, Integer, String, Float, Boolean, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 
@@ -9,7 +7,7 @@ Base = declarative_base()
 class Aircraft(Base):
     __tablename__ = 'aircraft'
 
-    id = Column(Integer, primary_key=True)
+    aircraft_id = Column(Integer, primary_key=True)
     icao = Column(String, unique=True, nullable=False)
     registration = Column(String)
     type = Column(String)
@@ -18,8 +16,8 @@ class Aircraft(Base):
 class Position(Base):
     __tablename__ = 'positions'
 
-    id = Column(Integer, primary_key=True)
-    aircraft_id = Column(Integer, ForeignKey('aircraft.id'), nullable=False)
+    position_id = Column(Integer, primary_key=True)
+    aircraft_id = Column(Integer, ForeignKey('aircraft.aircraft_id'), nullable=False)
     timestamp = Column(Float, nullable=False)  # Assuming UNIX timestamp here
     latitude = Column(Float)
     longitude = Column(Float)
@@ -28,8 +26,8 @@ class Position(Base):
 class Statistic(Base):
     __tablename__ = 'statistics'
 
-    id = Column(Integer, primary_key=True)
-    aircraft_id = Column(Integer, ForeignKey('aircraft.id'), nullable=False)
+    statistics_id = Column(Integer, primary_key=True)
+    aircraft_id = Column(Integer, ForeignKey('aircraft.aircraft_id'), nullable=False)
     max_altitude_baro = Column(Integer)
     max_ground_speed = Column(Float)
     had_emergency = Column(Boolean)
